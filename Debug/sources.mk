dac94e4e75fa544e7d97b85514f1efa060f80299
super-T-23 <76044594+super-T-23@users.noreply.github.com>
2021-11-12 20:35:48 +0800

    Add files via upload

---
+++ Debug/sources.mk
@@ -0,0 +1,32 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+ELF_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+SIZE_OUTPUT := 
+OBJDUMP_LIST := 
+EXECUTABLES := 
+OBJS := 
+S_DEPS := 
+S_UPPER_DEPS := 
+C_DEPS := 
+OBJCOPY_BIN := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Core/Src \
+Core/Startup \
+Drivers/BSP/B-L475E-IOT01 \
+Drivers/BSP/Components/M24SR \
+Drivers/BSP/Components/hts221 \
+Drivers/BSP/Components/lis3mdl \
+Drivers/BSP/Components/lps22hb \
+Drivers/BSP/Components/lsm6dsl \
+Drivers/STM32L4xx_HAL_Driver/Src \
+
